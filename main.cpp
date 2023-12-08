92b5bea0f7ea6c67987e8aafa335e83061d1a785
adi-geek2 <adi2271188@gmail.com>
2023-12-08 16:12:54 +0100

    Add public inheritance experiment

--- main.cpp
+++ main.cpp
@@ -1,14 +1,37 @@
-#include "facade.hpp"
+#include "PrivateInheritance.hpp"
 #include <iostream>
+
+using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    //Call facade
-    ArmorBuilderFacade abf;
+    Position p;
+    Velocity v;
+    cout << "X velocity before= " << v.v_x << endl;
+    v.v_x = 100.0F;
+    cout << "X velocity assigned before= " << v.v_x << endl;
+    cout << "X before = " << p.x << endl;
 
-    std::cout << abf.GetHeavyArmor() << std:: endl;
-    std::cout << abf.GetMediumArmor() << std:: endl;
-    std::cout << abf.GetLightArmor() << std:: endl;
+    Position p1(1.0F,1.0F);
+    Velocity v1(10.0F,10.0F);
+
+    p = p1;
+    v = v1;
+
+    cout << "X velocity = " << v.v_x << endl;
+    v.v_x = 1.0F;
+    cout << "X velocity assigned= " << v.v_x << endl;
+    cout << "X = " << p.x << endl;
+
+    Position p3(3.0F,3.0F);
+    Velocity v3(30.0F,30.0F);
+    v3.x = p3.x;  
+    
+    Position p2(p3);
+    Velocity v2(v3);
+
+    cout << "X after other= " << v2.x << endl;
+    v2.v_x = 1.0F;
 
     return 0;
 }
